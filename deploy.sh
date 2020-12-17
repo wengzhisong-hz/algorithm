2b26c241e10f3bdda1e268c95ffa2150a18f2723
sp0140(翁智松) <wengzs@supaur.com>
2020-12-17 17:47:29 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
+npm run build
 git add .
 git commit -m 'deploy'
 git push -f git@github.com:wengzhisong-hz/leetcode.git master
 
-git branch -D gh-pages || git checkout gh-pages
-git checkout gh-pages || git checkout -b gh-pages
-npm run build
 cd docs/.vuepress/dist
 git init
 git add -A
