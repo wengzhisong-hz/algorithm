dc6ebd37347968d69f2c0d7ffff622d33f2798f7
sp0140(翁智松) <wengzs@supaur.com>
2020-12-17 17:33:52 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
     title: '算法笔记',
     description: '算法笔记 leetcode',
     themeConfig: {
+        nav: [
+            { text: 'github', link: 'https://github.com/wengzhisong-hz' },
+            { text: '个人博客', link: 'https://wengzhisong-hz.github.io/algorithm/' }
+        ],
         sidebar: 'auto'
     }
 }
